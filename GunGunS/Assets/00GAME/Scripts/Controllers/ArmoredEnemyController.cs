6a6e237a91ebbdee7f3b5138c232237b45f37a95
Tien Dat <scd239@gmail.com>
2024-06-30 13:54:30 +0700

    GunGun

---
+++ GunGunS/Assets/00GAME/Scripts/Controllers/ArmoredEnemyController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmoredEnemyController : EnemyController
+{
+    [SerializeField] GameObject _armor;
+    bool _hasArmor;
+
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    public override void Init(Vector2 pos, int dir)
+    {
+        base.Init(pos, dir);
+        _coin = 2;
+        _coins = Random.Range(3, 6);
+        _hasArmor = true;
+        _armor.SetActive(true);
+        Rigidbody2D armorRb = _armor.GetComponent<Rigidbody2D>();
+        armorRb.gravityScale = 0;
+        _armor.transform.localPosition = new Vector3(0, -0.16f, 0);
+        _armor.transform.rotation = new Quaternion(0, 0, 0, 0);
+
+    }
+
+    public override void Update()
+    {
+        base.Update();
+        if (_armor.transform.position.y < PlayerController.instance.transform.position.y - 5f)
+        {
+            _armor.gameObject.SetActive(false);
+        }
+    }
+
+    public override void TakeDamage(int hit)
+    {
+        if (_isDeath) return;
+
+        if (!PlayerController.instance.GetComponentInChildren<GunController>()._fireCorrectCheck)
+            return;
+        PlayerController.instance.GetComponentInChildren<GunController>().SetFalseCheckCorrect();
+
+        if (_hasArmor)
+        {
+            _hasArmor = false;
+            Rigidbody2D armorRb = _armor.GetComponent<Rigidbody2D>();
+            armorRb.gravityScale = 2;
+            armorRb.AddForce(Vector2.up * 200f);
+            armorRb.AddTorque(100f);
+            PlayerController.instance.shooted = false;
+        }
+        else
+        {
+            base.TakeDamage(hit);
+        }
+    }
+
+    public override void Death(int hit)
+    {
+        base.Death(hit);
+    }
+
+}
